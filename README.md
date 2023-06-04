ebecc4432e826402a7f4ce7dcc39dcdad5a3f048
Kory Porter <kory.porter@hotmail.com>
2023-06-04 17:18:59 +1000

    tidy code

---
+++ README.md
@@ -0,0 +1,23 @@
+# Displaying fan RPM on an OLED using an Attiny85
+
+-- Demo video --
+
+## Requirements
+
+1. Attiny85 development board (the one with USB + 5V regulator), [e.g.](https://www.ebay.com.au/itm/191572154213).
+2. Arduino 2 IDE (think this comes pre-installed with the boards manager, you could use the original IDE but you'll need to add ATTinyCore to the boards manager, [like this](https://github.com/SpenceKonde/ATTinyCore/blob/v2.0.0-devThis-is-the-head-submit-PRs-against-this/Installation.md))
+3. 3 pin DC Fan
+   1. I've used an old [PL80S12M](https://www.elecok.com/power-logic-pl80s12m-12v-0-14a-3wires-cooling-fan.html). It activates down to 5v so I could test in circuit without an external supply.
+4. 128x64 i2c OLED
+   1. I've used [this one](https://core-electronics.com.au/dual-colour-oled-display-128x64-yellow-and-blue.html)
+
+## References
+
+- http://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-2586-AVR-8-bit-Microcontroller-ATtiny25-ATtiny45-ATtiny85_Datasheet.pdf
+- https://www.openimpulse.com/blog/wp-content/uploads/wpsc/downloadables/DIGISPARK-PRODUCTION-REV3-Schematic.pdf
+- [ATTinyCore to add TinyAVR support to Arduino](https://github.com/SpenceKonde/ATTinyCore)
+- [OLED ASCII Library](https://github.com/greiman/SSD1306Ascii)
+
+## Note on Interrupt Pins
+
+The ATTiny85 development boards have a handful of components attached to some of the GPIO pins to enable sub programming and to provide a regulated 5V supply, this means that not all pins are suitable to be used an interrupt. I could not get pin 4 to operate in a stable manner as it is part of the USB circuitry. Pin 5 is not connected to anything, but I was unable to get an interrupt to trigger on this pin, unsure why - think it has something to do with that pin being used as the reset. Pins 0 and 2 are ideal candidates for interrupt triggers, but these are used for i2c (SDA and SCL respectively), so I could not use them. I had moderate success with pin 3 even though it's connected to a USB data line. I think this is because unlike pin 4, pin 3 in the development board is tied to 5V. So it is mostly stable, but occasionally some data on the USB line causes it to freak out a bit and the RPM measurement is incorrect - maybe if this was connected to a USB power supply (no data) then it would not be an issue. Because pin 3 is used in the usb circuitry, **IT NEEDS TO BE UNPLUGGED BEFORE** programming, otherwise it'll just hang.
--- main/main.ino
+++ main/main.ino
@@ -13,9 +13,12 @@ volatile uint32_t rising_edge_counter = 0;
 volatile uint32_t last_rec_period_time = 0;
 volatile uint32_t period_start_time = millis();
 
+float previous_rpm = 0.0;
+
 SSD1306AsciiWire oled;
 
-void setup() {
+void setup()
+{
   Wire.begin();
   Wire.setClock(400000L);
 
@@ -33,44 +36,60 @@ void setup() {
   oled.print("By Kory Porter");
   oled.set2X();
 
-  cli(); // clear any interrupts
+  cli();                         // clear any interrupts
   PCMSK |= (1 << INTERRUPT_PIN); // enable interrupts only on interrupt pin
-  MCUCR |= (1 << 3); // configure only to trigger interrupts on rising edge
-  GIMSK |= (1 << PCIE); // enable pin change interrupts
+  MCUCR |= (1 << 3);             // configure only to trigger interrupts on rising edge
+  GIMSK |= (1 << PCIE);          // enable pin change interrupts
   pinMode(INTERRUPT_PIN, INPUT); // has a physical pull up on breadboard
-  sei(); // enable global interrupts
-
+  sei();                         // enable global interrupts
 }
 
-void loop() {
+void loop()
+{
   // getting fontWidth here as 1 character does not equal 1 pixel
   // so fontWidth * amount of columns to move along
   oled.setCursor(oled.fontWidth() * 5, 2);
-  oled.clearToEOL();
 
-  if (last_rec_period_time == 0) {
-    return;
+  // if it's been over a second since our last update, fan is stalled
+  // if last_rec is 0, we haven't startedvv
+  if ((millis() - period_start_time) > 1000 || last_rec_period_time == 0)
+  {
+    oled.clearToEOL();
+    oled.print(0);
   }
+  else
+  {
+    float rpm = 60.0 / ((2 * last_rec_period_time) / 1000.0);
 
-  float rpm = 60.0 / ((2 * last_rec_period_time) / 1000.0);
-  oled.print(int(rpm));
+    // only print if rpm has changed
+    if (rpm != previous_rpm)
+    {
+      previous_rpm = rpm;
+      oled.clearToEOL();
+      oled.print(int(rpm));
+    }
+  }
 
   delay(500);
 }
 
-// ISR is a magic interrupt function for ATTiny85 (maybe all the x5 but not sure)
+// "ISR" is a magic interrupt function for ATTiny85 (maybe all the x5 but not sure)
 // Any interrupts on registered pins will trigger this ISR, I'm sure you
 // could check registers to see which pin triggered the interrupt but
 // I've only registered PB3
-ISR(PCINT0_vect) {
+ISR(PCINT0_vect)
+{
   // if we've already seen a rising edge, we've done a full period
   // so take note of timing, and reset
-  if (rising_edge_counter == 1) {
+  if (rising_edge_counter == 1)
+  {
     uint32_t now = millis();
     last_rec_period_time = now - period_start_time;
     period_start_time = now;
     rising_edge_counter = 0;
-  } else {
+  }
+  else
+  {
     rising_edge_counter += 1;
   }
 }
